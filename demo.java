1703644a613237ad7b43e607c1fda7831327c270
golkondasreeja_ <132775141+golkondasreeja@users.noreply.github.com>
2025-07-31 14:52:17 +0530

    fifth commit

--- demo.java
+++ demo.java
@@ -2,5 +2,6 @@ class demo{
 	public static void main(String args[]){
 		System.out.println("Hello World");
 		System.out.println("Welcome to Jenkins");
+		System.out.println("SCM polling to monitor change commits in github");
 	}
 }
