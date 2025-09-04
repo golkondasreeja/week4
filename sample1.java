876927b05b674403143947951161cc7bd5e038bb
golkondasreeja_ <132775141+golkondasreeja@users.noreply.github.com>
2025-09-04 15:15:18 +0530

    Update sample1.java

--- sample1.java
+++ sample1.java
@@ -1,5 +1,14 @@
-class main{
-	public static void main(String args[]){
-		System.out.printlm("Hello World");
-	}
+class Main {
+    // Function to multiply two numbers
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int product = multiply(a, b); // call function
+        System.out.println("The product of a and b is: " + product);
+    }
 }
